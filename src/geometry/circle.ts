616dfb513c7b72a4f41e781dd7e84213d26d4c51
David van der Sluijs <david@van-der-sluijs.de>
2022-07-27 18:31:21 +0200

    Initial commit

---
+++ src/geometry/circle.ts
@@ -0,0 +1,3 @@
+export function Circle() {
+    return 'test';
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Circle } from './geometry/circle';
+
+const Geometry = {
+    Circle
+}
+
+export const MathUtils = {
+    Geometry
+}
